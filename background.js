a4492097675b36f32d587f56eca69873cb980aac
Hamid Ahmadi <164649651+idkvarghastaken@users.noreply.github.com>
2024-07-18 14:27:18 +0330

    Add files via upload

---
+++ background.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const endpoint = 'invidious.privacyredirect.com';
+    if (!window.location.toString().includes(endpoint)) {
+        function walkText(node, text, replace) {
+            if (node.nodeType === 3 && text.test(node.data)) {
+                node.data = node.data.replace(text, replace);
+            } else if (node.nodeType === 1 && node.nodeName !== "SCRIPT" && !node.closest('[contenteditable="true"]')) {
+                Array.from(node.attributes).forEach(function (attr) {
+                    if (attr.specified && !(node.nodeName === "IMG" && attr.name === "src" || attr.name === "value")) {
+                        if (text.test(attr.value)) {
+                            attr.value = attr.value.replace(text, replace);
+                        }
+                    }
+                });
+
+                Array.from(node.childNodes).forEach(function (childNode) {
+                    walkText(childNode, text, replace);
+                });
+            }
+        }
+
+        function setup() {
+            walkText(document.body, /(www\.)?youtube\.com\/account/, endpoint + '/login');
+            walkText(document.body, /(www\.)?youtube\.com/, endpoint);
+            walkText(document.body, /youtube\.com/, endpoint);
+            walkText(document.body, /^https?:\/\/youtu.be/, 'https://');
+            walkText(document.body, /YouTube/, 'Invidious');
+            walkText(document.body, /youtube/, 'invidious');
+            walkText(document.body, /یوتیوب/, 'اینویدیوس');
+            walkText(document.body, /یوتوب/, 'اینویدیوس');
+            document.querySelectorAll('a').forEach((el) => {
+                if (el.href.includes('youtube.com/account')) {
+                    el.href = el.href.replace('youtube.com/account', endpoint + '/login');
+                } else if (el.href.includes('youtube.com')) {
+                    el.href = el.href.replace('youtube.com', endpoint);
+                } else if (el.href.includes('youtu.be')) {
+                    el.href = el.href.replace('youtu.be', endpoint);
+                }
+            });
+            document.querySelectorAll('iframe').forEach((el) => {
+                if (el.src.includes('youtube.com/account')) {
+                    el.src = el.src.replace('youtube.com/account', endpoint + '/login');
+                } else if (el.src.includes('youtube.com')) {
+                    el.src = el.src.replace('youtube.com', endpoint);
+                } else if (el.src.includes('youtu.be')) {
+                    el.src = el.src.replace('youtu.be', endpoint);
+                }
+            });
+        }
+
+        setup();
+        const observer = new MutationObserver(() => {
+            setup();
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+            mutations: [
+                {
+                    addedNodes: true,
+                },
+            ],
+        });
+    }
+});
